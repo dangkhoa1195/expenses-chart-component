bdf76241377fa2ab1c461ad0f88b0d0b3cd7e42d
dangkhoa1195 <ngdangkhoa1195@gmail.com>
2023-01-28 14:59:03 +0700

    update style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./dist/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'soft-red': 'hsl(10, 79%, 65%)',
+        cyan: 'hsl(186, 34%, 60%)',
+        'dark-brown': 'hsl(25, 47%, 15%)',
+        'medium-brown': 'hsl(28, 10%, 53%)',
+        cream: 'hsl(27, 66%, 92%)',
+        'very-pale-orange': 'hsl(33, 100%, 98%)',
+      },
+    },
   },
   plugins: [],
 };
